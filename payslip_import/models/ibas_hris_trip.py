86d05c7faa5d1bc7a7c909d6a24e0ec284f1cfd2
Lawrence <jovenlawrencegersaniba.fcu@gmail.com>
2020-11-15 20:45:51 +0800

    NDM 11-13-2020 - Request 1,2,3,4

--- payslip_import/models/ibas_hris_trip.py
+++ payslip_import/models/ibas_hris_trip.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class TripTemplate(models.Model):
@@ -13,3 +13,20 @@ class Trip(models.Model):
     _description = 'TRIPS'
 
     code = fields.Char(related='trip_template_id.code')
+    quantity = fields.Float(string='Quantity', default=1)
+    sub_amount = fields.Monetary(string="Subtotal Amount")
+    amount = fields.Monetary(string="Amount", required=True, compute='_compute_amount')
+
+    @api.onchange('trip_template_id')
+    def _onchange_sub_amount(self):
+        if self.trip_template_id:
+            self.sub_amount = self.trip_template_id.amount
+
+    @api.depends('quantity', 'sub_amount')
+    def _compute_amount(self):
+        for rec in self:
+            if rec.quantity > 0:
+                rec.amount = rec.quantity * rec.sub_amount
+            else:
+                rec.quantity = 0
+
